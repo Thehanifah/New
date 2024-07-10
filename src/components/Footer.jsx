3aaf053fe8600198395be74d7bd3199c6707321f
The Hanifah <hanifah2015@gmail.com>
2024-07-10 18:18:12 +0100

    new changes again

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import './Footer.css'
 import Logo from '../images/logo.png'
 import { PiCopyrightLight } from "react-icons/pi";
-import { BsTwitterX } from "react-icons/bs";
+import { FaXTwitter } from "react-icons/fa6";
 import { IoLogoInstagram } from "react-icons/io";
 import { FaWhatsapp } from "react-icons/fa6";
 import { RiTiktokFill } from "react-icons/ri";
@@ -12,51 +12,58 @@ function Footer() {
   return (
     <>
       <div className="container-fluid brown"></div> 
-      <div class=" row d-md-flex pb-10 mt-10 text-center footer-cont">
-     <div class="col-12 col-md-4 p-2 footerbrand"> 
+
+
+      <div class="footer-cont">
+     <div class="footerbrand"> 
      <a className="navbar-brand" href="">
         <img src={Logo} alt="Logo" width="auto" height="40" className="d-inline-block align-text-center" />
         <span className="d-inline-block align-text-top ">RareWrld</span>
       </a>
      </div>
      
-      <div class=" col-12 col-md-4  ms-auto p-2 text-center ul-container"> <ul class="list-inline">
-  <li class="list-inline-item">
-  <a href="#" class="link-primary link-offset-2 link-underline-opacity-0 text-lg-end">Terms and Conditions</a></li>
-  <li class="list-inline-item">
-  <a href="#" class="link-primary link-offset-2 link-underline-opacity-0 text-lg-end">Privacy</a></li>
-  <li class="list-inline-item">
-  <a href="#" class="link-primary link-offset-2 link-underline-opacity-0 text-lg-end">Cookies</a></li>
-  <li class="list-inline-item">
-  <a href="#" class="link-primary link-offset-2 link-underline-opacity-0 text-lg-end">Refund</a></li>
-  <li class="list-inline-item">
-  <a href="#" class="link-primary link-offset-2 link-underline-opacity-0 text-lg-end">License</a></li>
-</ul>
-      
-      </div>
+    <div class="ul-container"> 
+  
+    <div><a href="#" >Terms and Conditions</a></div> 
+   
+    <div><a href="#" >Privacy</a></div>
+  
+     <div><a href="#" >Cookies</a></div>
+  
+     <div><a href="#" >Refund</a></div>
+  
+    <div><a href="#" >License</a></div>
+    </div>
     </div>
 
+  
+  
+  
+  
   <hr className='hro' />
-  <div class="row copyright">
-    <div class="col-auto me-auto copyright-word"><PiCopyrightLight className='copyright-logo'/> Copyright 2024  RareWrld</div>
-    <div class="col-auto ul-container"> <ul class="list-inline">
-  <li class="list-inline-item">
-  <a href="#" class="link-primary link-offset-2 link-underline-opacity-0 text-lg-end"><IoLogoInstagram /></a></li>
-  <li class="list-inline-item">
-  <a href="#" class="link-primary link-offset-2 link-underline-opacity-0 text-lg-end"><BsTwitterX /></a></li>
-  <li class="list-inline-item">
-  <a href="#" class="link-primary link-offset-2 link-underline-opacity-0 text-lg-end"><FaWhatsapp /></a></li>
-  <li class="list-inline-item">
-  <a href="#" class="link-primary link-offset-2 link-underline-opacity-0 text-lg-end"><RiTiktokFill /></a></li>
 
-</ul> </div>
+
+  <div class="copyright">
+    <div class="copyright-word"><PiCopyrightLight className='copyright-logo'/> Copyright 2024  RareWrld</div>
+
+    <div class=" ul-container1"> 
+ 
+  <a href="#" ><IoLogoInstagram className='media' /></a>
+ 
+  <a href="#" ><FaXTwitter className='media' /></a>
+ 
+  <a href="#" ><FaWhatsapp className='media' /></a>
+ 
+  <a href="#" ><RiTiktokFill className='media'/></a>
+
+ </div>
   </div>
       
     </>
   )
 }
 
-export default Footer
+export default Footer;
 
 
 {/* <div class="d-flex mb-3">
